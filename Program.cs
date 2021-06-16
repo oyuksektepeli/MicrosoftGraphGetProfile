45038970cef566426a77a65e5df834563a334c1f
Onur Yüksektepeli <onuryuksektepeli@hotmail.com.tr>
2021-06-16 03:52:36 +0300

    initial commit

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Identity.Client;
+using System;
+using Microsoft.Graph;
+using Microsoft.Graph.Auth;
+
+namespace MicrosoftGraphGetProfile
+{
+    class Program
+    {
+        private const string _clientId = "";
+        private const string _tenantId = "";
+
+        public static async Task Main(string[] args)
+        {
+            var app = PublicClientApplicationBuilder
+            .Create(_clientId)
+            .WithAuthority(AzureCloudInstance.AzurePublic, _tenantId)
+            .WithRedirectUri("http://localhost")
+            .Build();
+
+            string[] scopes = { "user.read" };
+            
+            var provider = new InteractiveAuthenticationProvider(app, scopes);
+            var client = new GraphServiceClient(provider);
+            User me = await client.Me.Request().GetAsync();
+            Console.WriteLine($"Display Name:\t{me.DisplayName}");
+
+
+
+        }
+    }
+}
